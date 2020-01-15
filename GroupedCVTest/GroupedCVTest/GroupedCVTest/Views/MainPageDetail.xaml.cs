9310d44bd0d69ec33c95646a9e6a1333b661da1d
AR <Wizardskills@gmail.com>
2020-01-15 16:28:16 +0000

    Simple Sample Added

---
+++ GroupedCVTest/GroupedCVTest/GroupedCVTest/Views/MainPageDetail.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GroupedCVTest.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPageDetail : ContentPage
+    {
+        public List<DisplayGroupItem> Items { get; set; } = new List<DisplayGroupItem>();
+
+        public MainPageDetail()
+        {
+            InitializeComponent();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                List<BasicItem> items = new List<BasicItem>();
+
+                for (int j = 1; j <= 6; j++)
+                {
+                    items.Add(new BasicItem() { ItemText = j.ToString() });
+                }
+
+                DisplayGroupItem dgi = new DisplayGroupItem(i.ToString(), items);                
+
+                Items.Add(dgi);
+            }
+
+            BindingContext = this;
+        }
+    }
+
+    public class DisplayGroupItem : List<BasicItem>
+    {
+        public string Name { get; set; }
+
+        public DisplayGroupItem(string name, List<BasicItem> basicItems) : base(basicItems)
+        {
+            Name = name;
+        }
+    }
+
+    public class BasicItem
+    {
+        public string ItemText { get; set; }
+    }
+}
